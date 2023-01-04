84d821429f99212a1cc8ed97b7132eed35bbff82
ViDA_TG <victorademiju.t@gmail.com>
2023-01-04 15:30:09 +0100

    Commit V1.01

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+mongoose.connect('mongodb://127.0.0.1:27017/myapp', {useNewUrlParser: true});
+
 app.listen(8800, () => {
     console.log("Backend started");
 })
