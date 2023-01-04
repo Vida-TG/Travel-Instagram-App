15f808f3818b4173ec3ec8c864e256a7fba7d7a3
ViDA_TG <victorademiju.t@gmail.com>
2023-01-04 17:06:14 +0100

    Models

--- routes/pins.js
+++ routes/pins.js
@@ -3,6 +3,13 @@ const Pin = require(".../models/Pin");
 
 // Create a pin
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
     const newPin = new Pin(req.body);
+
+    try{
+        const savedPin = await newPin.save();
+        res.status(200).json(savedPin)
+    }catch(err){
+        res.status(500).json(err);
+    }
 })
