c00d36bb731db59b93679a183479122e72f375c1
ViDA_TG <victorademiju.t@gmail.com>
2023-01-04 16:20:45 +0100

    Mongoose connection

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const dotenv = require("dotenv");
 const app = express();
 
-mongoose.connect('mongodb://127.0.0.1:27017/myapp', {useNewUrlParser: true});
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true}).then(()=>{
+    console.log("MONGO Connected");
+}).catch((err)=>{
+    console.log(err);
+});
 
 app.listen(8800, () => {
     console.log("Backend started");
