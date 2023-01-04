23240daa692b33c2de9da505c63345d3dc9a164c
ViDA_TG <victorademiju.t@gmail.com>
2023-01-04 16:56:34 +0100

    Models

---
+++ models/Pin.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const PinSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        require: true,
+        min: 3
+    },
+    title: {
+        type: String,
+        min: 3,
+        require: true
+    },
+    desc: {
+        type: String,
+        min: 3,
+        require: true
+    },
+    rating: {
+        type: Number,
+        require: true,
+        min: 0,
+        max: 5
+    },
+    lat: {
+        type: Number,
+        require: true
+    },
+    long: {
+        type: Number,
+        require: true
+    }
+},
+{
+    timestamps: true
+});
+
+modules.exports = mongoose.model("Pin", PinSchema)
---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        require: true,
+        min: 3,
+        max: 20,
+        unique: true
+    },
+    email: {
+        type: String,
+        max: 50,
+        require: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        require: true,
+        min: 6
+    },
+},
+{
+    timestamps: true
+});
+
+modules.exports = mongoose.model("User", UserSchema)
